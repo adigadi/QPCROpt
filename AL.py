2f32cd6c9a5c949cef4b7668b2717b0271a65cc8
amoebacodes <67404973+amoebacodes@users.noreply.github.com>
2022-03-29 10:57:09 -0400

    Update AL.py

--- AL.py
+++ AL.py
@@ -45,7 +45,7 @@ def write_query_file(output_filename, observed_file, unobserved_file, batch_size
     #  batch 3 parameter set (samples) for us to "query.txt"
     query_index = get_query_index(observed_file, unobserved_file, batch_size)
     unobserved = pd.read_csv(unobserved_file)
-    toWrite = unobserved.iloc[query_index, :].to_csv(index = False)
+    toWrite = unobserved.iloc[query_index, :].to_csv(index = True)
     with open(output_filename, "w") as f:
         f.write(toWrite)
         f.close()
