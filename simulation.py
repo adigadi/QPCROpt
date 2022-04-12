e46455c2c402abdd6b913fb50bff6b870d3cb237
Adi Gadi <aditya.gadiyar@gmail.com>
2022-04-12 12:34:16 -0400

    made another observed file

--- simulation.py
+++ simulation.py
@@ -6,9 +6,9 @@ def main():
     
     AL.write_qPCR_output("qPCR_output.txt") # read qPCR spreadsheet and write to txt file
 
-    AL.update_observed_file("observed.txt", "query.csv", "qPCR_output.txt", False) # read queries, add CT values, and add queries to observed file
+    AL.update_observed_file("obsSim.txt", "query.csv", "qPCR_output.txt", False) # read queries, add CT values, and add queries to observed file
     AL.update_unobserved_file("unobsSim.txt", "query.csv") # remove recently updated rows from unobserved file
-    AL.write_query_file("query.csv", "observed.txt", "unobsSim.txt", 3, False) # update model and pick new queries
+    AL.write_query_file("query.csv", "obsSim.txt", "unobsSim.txt", 3, False) # update model and pick new queries
 
 
 if __name__ == "__main__":
