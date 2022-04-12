7f2ebed557003cdf4df977378d82017c15083951
Adi Gadi <aditya.gadiyar@gmail.com>
2022-04-12 11:26:16 -0400

    simulation calls done, need init

--- simulation.py
+++ simulation.py
@@ -1,26 +1,15 @@
-'''
-Pick 3 random points as first query
-Write those indices to query_filename
-
-Read entire dataset from unobserved_filename as unobserved
-Pick 3 most uncertain to query
-Drop indices from unobserved -> update unobserved
-
-
-'''
-
 import AL
-import init
+
 
 def main():
     
-    # init.init("simDataGC.txt", 3, "query.csv", True)
+    # init.init("simDataGC.txt", 3, "query.csv", True) # pick 3 initial queries
 
-    AL.write_qPCR_output("qPCR_output.txt")
+    AL.write_qPCR_output("qPCR_output.txt") # read qPCR spreadsheet and write to txt file
 
-    AL.update_observed_file("observed.txt", "query.csv", False)
-    AL.update_unobserved_file("unobserved.txt", "query.csv")
-    AL.write_query_file("query.csv", "observed.txt", "unobserved.txt", 3, False)
+    AL.update_observed_file("observed.txt", "query.csv", False) # read queries, add CT values, and add queries to observed file
+    AL.update_unobserved_file("unobserved.txt", "query.csv") # remove recently updated rows from unobserved file
+    AL.write_query_file("query.csv", "observed.txt", "unobserved.txt", 3, False) # update model and pick new queries
 
 
 if __name__ == "__main__":
