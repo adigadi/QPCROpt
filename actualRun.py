9670e8f5654238d5925e4b2815f1e24d27117b2a
Adi Gadi <aditya.gadiyar@gmail.com>
2022-04-12 12:28:27 -0400

    fixed actualRun

--- actualRun.py
+++ actualRun.py
@@ -5,9 +5,9 @@ def main():
     
     AL.write_qPCR_output("qPCR_output.txt") # read qPCR spreadsheet and write to txt file
 
-    AL.update_observed_file("observed.txt", "query.csv", False) # read queries, add CT values, and add queries to observed file
+    AL.update_observed_file("observed.txt", "query.csv", "qPCR_output.txt", False) # read queries, add CT values, and add queries to observed file
     AL.update_unobserved_file("unobserved.txt", "query.csv") # remove recently updated rows from unobserved file
-    AL.write_query_file("query.csv", "observed.txt", "unobserved.txt", 3, False) # update model and pick new queries
+    AL.write_query_file("query.csv", "observed.txt", "unobserved.txt", 12, False) # update model and pick new queries
 
 
 if __name__ == "__main__":
