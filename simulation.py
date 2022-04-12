177080f0e0c3f9b0046b9a717000102140fe608e
Adi Gadi <aditya.gadiyar@gmail.com>
2022-04-12 12:27:30 -0400

    fixing args to update_observed

--- simulation.py
+++ simulation.py
@@ -6,7 +6,7 @@ def main():
     
     AL.write_qPCR_output("qPCR_output.txt") # read qPCR spreadsheet and write to txt file
 
-    AL.update_observed_file("observed.txt", "query.csv", False) # read queries, add CT values, and add queries to observed file
+    AL.update_observed_file("observed.txt", "query.csv", "qPCR_output.txt", False) # read queries, add CT values, and add queries to observed file
     AL.update_unobserved_file("unobsSim.txt", "query.csv") # remove recently updated rows from unobserved file
     AL.write_query_file("query.csv", "observed.txt", "unobsSim.txt", 3, False) # update model and pick new queries
 
